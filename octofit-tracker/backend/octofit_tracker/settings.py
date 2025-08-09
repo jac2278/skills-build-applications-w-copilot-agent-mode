9b7b740741280aa313929042cbd9c6e4dd78fac0
jac2278 <jacob22788@gmail.com>
2025-08-09 10:11:25 +0000

    Update codespace name to settings.py file

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -131,3 +131,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Codespace Django REST API endpoint suffix
+CODESPACE_API_SUFFIX = '/api/'
